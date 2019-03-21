e7eed3d4008bc936f14f4b6ed83797da2bef14b9
Juan Castelli <juancastelli12345@gmail.com>
2019-03-21 19:23:16 -0300

    despues de punto 2

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -3,14 +3,23 @@
 #define N 4
 #define M 5
 
-int f,c;
-double mt [N][M];
-
-for (f = 0; f < N; ++f)
-{
-	for (c = 0; c < M; ++c)
+int main(void){
+	int f,c;
+	double mt[N][M];
+	for (f = 0; f < N; ++f)
 	{
-		printf("%lf\n", mt[f][c]);
+		for (c = 0; c < M; ++c)//Poner valores de matriz en 0
+		{
+			mt[f][c]=0;
+		}
 	}
-	printf("\n");
+
+	for (f = 0; f < N; ++f)
+	{
+		for (c = 0; c < M; ++c)
+		{
+			printf("%lf ", mt[f][c]);//Para que salga ordenada
+		}
+		printf("\n");
+	}
 }
